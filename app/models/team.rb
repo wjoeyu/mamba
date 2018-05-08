788e0d7f094d5c0aae04656723075c9594d028dc
App Academy Student <wjoeyu@gmail.com>
2018-05-08 15:01:25 -0400

    added session_api_util

---
+++ app/models/team.rb
@@ -0,0 +1,6 @@
+class Team < ApplicationRecord
+
+  validates :team_name, presence: true
+
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508163607) do
+ActiveRecord::Schema.define(version: 20180508182004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: :cascade do |t|
+    t.string "team_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
