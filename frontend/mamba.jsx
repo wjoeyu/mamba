9d2b7563dcf37316959736c57be3f4a90f3e8296
App Academy Student <wjoeyu@gmail.com>
2018-05-08 17:54:48 -0400

    added spaces

--- frontend/mamba.jsx
+++ frontend/mamba.jsx
@@ -5,7 +5,7 @@ import configureStore from './store/store';
 document.addEventListener('DOMContentLoaded', () => {
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Mamba</h1>, root);
+  ReactDOM.render(<h1>M a m b a</h1>, root);
 
   // const store = configureStore();
   // window.getState = store.getState;
