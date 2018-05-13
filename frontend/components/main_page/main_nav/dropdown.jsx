993299154a7c68df9351663ec1c36b44867a93ac
Andrew Schumacher <as228512@ohio.edu>
2018-05-12 20:26:25 -0400

    got dropdown to work

--- frontend/components/main_page/main_nav/dropdown.jsx
+++ frontend/components/main_page/main_nav/dropdown.jsx
@@ -4,10 +4,19 @@ import { withRouter } from 'react-router-dom';
 export class Dropdown extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      visible: false,
+    };
+    this.toggleVisibility = this.toggleVisibility.bind(this)
   }
 
   componentDidMount() {
     this.props.fetchCurrentTeams();
+  }
+
+  toggleVisibility() {
+    const currentState = this.state.visible;
+    this.setState( {visible: !currentState} );
   }
 
   render() {
@@ -19,15 +28,17 @@ export class Dropdown extends React.Component {
 
     return (
       <div className="dropdown">
-        <div className="dropdown-button">
+        <div className="dropdown-button" onClick={this.toggleVisibility}>
           Click Me!
         </div>
-        <div className="dropdown-list">
-          {currentTeams}
-        <div>
-          <button onClick={() => this.props.logout().then(() => this.props.history.push('/'))}>Log Out</button>
-        </div>
-        </div>
+          <div className={this.state.visible ?
+              "dropdown-list" : "dropdown-hidden"}>
+            {currentTeams}
+            <div>Workplace settings...</div>
+
+            <button onClick={() => this.props.logout().then(() =>
+                this.props.history.push('/'))}>Log Out</button>
+          </div>
       </div>
     );
   }
