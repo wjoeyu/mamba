ab0f0e0ed3f4ccde5e086240fe8d547c7e60ebdc
Ryan <rnowusu@gwu.edu>
2018-05-13 20:56:56 -0400

    added team util functions

--- frontend/util/teams_api_util.jsx
+++ frontend/util/teams_api_util.jsx
@@ -5,9 +5,48 @@ export const getUserTeams = () => {
   });
 };
 
+
 export const getTeamMembers = (teamId) => {
   return $.ajax({
     method: 'GET',
     url: `/api/teams/${teamId}/`
+  });
+};
+
+export const addTeamMembers = (team) => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `api/teams/${team.id}`,
+    data: { team }
+  });
+};
+
+export const getTeam = (teamId) => {
+  return $.ajax({
+    method: 'GET',
+    url: `api/teams/${teamId}`
+  });
+};
+
+export const createTeam = (team) => {
+  return $.ajax({
+    method: 'POST',
+    url: 'api/teams/',
+    data: { team }
+  });
+};
+
+export const updateTeam = (team) => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `api/teams/${team.id}`,
+    data: { team }
+  });
+};
+
+export const removeMember = (teamId) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `api/teams/${teamId}`
   });
 };
